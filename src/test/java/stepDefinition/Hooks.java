4715bc8a90761f4eceb2ca24b26b98ffce559eea
Alex Cândido <alex.candido@liferay.com>
2020-12-10 02:42:16 -0300

    Adding Hooks class containing @Before/@After calls to be executed before/after each scenario

--- src/test/java/stepDefinition/Hooks.java
+++ src/test/java/stepDefinition/Hooks.java
@@ -1,2 +1,18 @@
-package stepDefinition;public class Hooks {
+package stepDefinition;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import manager.AppiumDriverManager;
+
+public class Hooks {
+
+    @Before
+    public static void setUp(){
+        AppiumDriverManager.getDriver();
+    }
+
+    @After
+    public static void tearDown(){
+        AppiumDriverManager.closeDriver();
+    }
 }
