ac0d75c5f95e04f4855099b80715f1eaa999022b
Alex Cândido <alex.candido@liferay.com>
2020-12-10 02:26:46 -0300

    Source format

--- src/test/java/runner/CucumberTestRunner.java
+++ src/test/java/runner/CucumberTestRunner.java
@@ -6,11 +6,23 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-html-report"},
-        features = {"src/main/resources/features"},
-        glue = {"stepDefinition"},
-        monochrome = true
-        )
+        // Run the scenarios specified by tags
+        //tags = {""},
 
-public class CucumberTestRunner {
+        // Define the features files location
+        features = {"src/main/resources/features"},
+
+        // Define the steps files location
+        glue = {"stepDefinition"},
+
+        // Readable format of the console output from Cucumber
+        monochrome = true,
+
+        // Code snippets will be created using camelcase
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+
+        // Plugins
+        plugin = {"pretty", "html:target/cucumber-html-report"}
+        )
+class CucumberTestRunner {
 }
