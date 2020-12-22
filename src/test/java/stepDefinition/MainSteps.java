a204f424fcb8f5be518baa1515e04520107d2c64
Alex Cândido <alex.candido@liferay.com>
2020-12-22 15:29:16 -0300

    Adding MainSteps

--- src/test/java/stepDefinition/MainSteps.java
+++ src/test/java/stepDefinition/MainSteps.java
@@ -1,6 +1,36 @@
 package stepDefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import manager.AppiumDriverManager;
+import pages.MainPage;
+
 public class MainSteps {
 
+    MainPage mainPage = new MainPage(AppiumDriverManager.getDriver());
 
+    @Then("I see the result is completed")
+    public void resultIsCompleted() {
+        assert mainPage.percentStr.isDisplayed();
+    }
+
+    @Then("I see there is a candidate elected")
+    public void candidateElected() {
+        assert mainPage.electedStr.isDisplayed();
+    }
+
+    @Given("I open one of the candidates")
+    public void openCandidate() {
+        mainPage.firstCandidateAvatar.click();
+    }
+
+    @Given("I click on favorite tab")
+    public void openFavoriteTab() {
+        mainPage.favoriteTab.click();
+    }
+
+    @Given("I see the candidate is present")
+    public void favoriteListIsNotEmpty() {
+        assert mainPage.electedStr.isDisplayed();
+    }
 }
